6246267752364bb5b2451285ba9fd1807f63df65
GaGiiiii <dragoslav.gagi8@gmail.com>
2021-10-24 12:44:36 +0200

    Commit posle nestanka struje

--- students.php
+++ students.php
@@ -13,11 +13,58 @@ function clean($input) {
   return $input;
 }
 
+function calculatePassCSM($student) {
+  $grades = $student['students.grades'];
+  $grades = explode(',', $grades);
+  $avg = array_sum($grades) / count($grades);
+
+  return $avg >= 7 ? ['avg' => $avg, 'pass' => 'Pass'] : ['avg' => $avg, 'pass' => 'Fail'];
+}
+
+function calculatePassCSMB($student) {
+  $grades = $student['students.grades'];
+  $grades = explode(',', $grades);
+  $avg = array_sum($grades) / count($grades);
+
+  return $avg >= 7 ? ['avg' => $avg, 'pass' => 'Pass'] : ['avg' => $avg, 'pass' => 'Fail'];
+}
+
 $student = Database::getInstance()->getStudent(clean($_GET['id']));
 
-if(!$student){
+if (!$student) {
   echo "Student not found";
   die();
 }
 
 $school = Database::getInstance()->getSchool($student['students.school_id']);
+$pass = false;
+$result = [];
+
+switch ($school['schools.name']) {
+  case 'CSM':
+    $passArr = calculatePassCSM($student);
+
+    $result['student_id'] = $student['students.id'];
+    $result['name'] = $student['students.name'];
+    $result['grades'] = $student['students.grades'];
+    $result['average'] = $passArr['avg'];
+    $result['final'] = $passArr['pass'];
+
+    $result = json_encode($result);
+    break;
+  case 'CSMB':
+    $passArr = calculatePassCSMB($student);
+
+    $result['student_id'] = $student['students.id'];
+    $result['name'] = $student['students.name'];
+    $result['grades'] = $student['students.grades'];
+    $result['average'] = $passArr['avg'];
+    $result['final'] = $passArr['pass'];
+
+    $result = json_encode($result);
+    break;
+  default:
+    echo 'Error';
+}
+
+echo $result;
